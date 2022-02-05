45f3f64f0a05d081fb18282a47e57ae7608111f7
time-shimizu <213.f.r48132@gmail.com>
2022-02-05 17:18:09 +0900

    use lateinit to increase performance

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
     }
@@ -17,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 //        Toast.makeText(this, "button clicked",
 //            Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
