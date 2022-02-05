54f0d4885ff63897893f64702451ae9f80834421
time-shimizu <213.f.r48132@gmail.com>
2022-02-05 09:30:32 +0900

    add countUp button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
@@ -20,5 +22,16 @@ class MainActivity : AppCompatActivity() {
         var randomInt = (1..6).random()
         var resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private  fun countUp() {
+        var resultText: TextView = findViewById(R.id.result_text)
+        if (resultText.text == "Hello World!") {
+            resultText.text = 1.toString()
+        } else if (resultText.text.toString().toInt() >= 6) {
+            return
+        } else {
+            resultText.text = (resultText.text.toString().toInt() + 1).toString()
+        }
     }
 }
